0aaf08e94b0a51325cc0cc912aff1b3ddedf341e
Lynnnnaa <lxsxujiarong@outlook.com>
2019-11-21 20:51:39 -0700

    profile view

---
+++ easyGrading/Models/profileModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace easyGrading.Models
+{
+    public class ProfileModel
+    {
+        public string Username {get; set;}
+        public string Degree {get; set;}
+        public string Major {get; set;}
+        public string Minor {get; set;}
+    }
+}
