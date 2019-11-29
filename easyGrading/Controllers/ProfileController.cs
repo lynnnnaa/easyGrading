81d622e468fcd0fca2d1770fc29e46f33970c8c7
Lynnnnaa <lxsxujiarong@outlook.com>
2019-11-29 00:28:18 -0700

    updated profile, the link need to be added under main screen page

---
+++ easyGrading/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using easyGrading.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyGrading.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult ProfileView()
+        {
+
+            var model = new ProfileModel()
+            {
+                DepartmentName = "Bachelor of Science",
+                Major = "Computer Science(Major)",
+                Minor = "Mathematics(Minor)",
+                StudentName = "Eyal ZuriMy"
+            };
+            return View(model);
+        }
+
+    }
+}
